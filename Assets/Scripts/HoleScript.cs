23189c3dda5e99afe9116bfde4ebf2ca9d9ccd5a
Kevin <kevin.zhong94@gmail.com>
2015-03-27 23:30:39 +0700

    hancurkan penghalang

---
+++ Assets/Scripts/HoleScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Dirt")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
