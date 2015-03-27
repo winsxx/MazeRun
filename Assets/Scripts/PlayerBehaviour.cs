68b8fd91353b7f52321b7815f12d20e11f30e8eb
winsxx <13512071@std.stei.itb.ac.id>
2015-03-27 09:43:13 +0700

    Update Enemy AI

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
+	public int startingHealth = 100;
+	public int currentHealth;
+	//public Slider healthSlider;
+	//public Image damageImage;
+	public AudioClip deathClip;
+	public float flashSpeed = 5f;
+	public Color flashColour = new Color (1f, 0f, 0f, 0.1f);
+
+	Animator anim;
+	AudioSource playerAudio;
+	//PlayerMovement playerMovement;
+	//PlayerShooting playerShooting;
+	bool damaged;
+	bool isDead;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		playerAudio = GetComponent<AudioSource> ();
+		//playerMovement = GetComponent<PlayerMovement> ();
+		//playerShooting = GetComponentInChildren<PlayerShooting> ();
+		currentHealth = startingHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (damaged) {
+			//damageImage.color = flashColour;
+		} else {
+			//damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
+
+	public void TakeDamage(int amount){
+		damaged = true;
+
+		currentHealth -= amount;
+		//healthSlider.value = currentHealth;
+		playerAudio.Play ();
+
+		if (currentHealth <= 0 && !isDead) {
+			Death();
+		}
+	}
+
+	void Death(){
+		isDead = true;
+		//playerShooting.DisableEffects ();
+		//anim.SetTrigger ("Die");
+		//playerAudio.clip = deathClip;
+		playerAudio.Play ();
+		//playerMovement.enabled = false;
+		//playerShooting.enabled = false;
 	}
 }
