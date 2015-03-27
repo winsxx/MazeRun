31e9a6013667d673be09994fdf224a5e0d81e766
Kevin <kevin.zhong94@gmail.com>
2015-03-27 17:17:28 +0700

    Throw

---
+++ Assets/UnityChan/Scripts/ThrowScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowScript : MonoBehaviour {
+
+    public GameObject Bomb;
+    public GameObject Dirt;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            Instantiate(Bomb, transform.position + 7.0f * transform.forward, transform.rotation);
+        }
+        else if (Input.GetButton("Fire2"))
+        {
+            Instantiate(Dirt, transform.position + 7.0f * transform.forward, transform.rotation);
+        }
+	}
+}
