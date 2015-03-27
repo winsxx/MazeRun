64e55f36ed0933a77fb492c39d3f3ea664b16fda
winsxx <13512071@std.stei.itb.ac.id>
2015-03-27 17:48:16 +0700

    Add shot magic animation

---
+++ Assets/ShotMagicController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotMagicController : MonoBehaviour {
+	public GameObject magicBall;
+	public float spawnDistance = 1.0f;
+	public float delay = 1000000f;
+	private float currentDelay = 0;
+
+	// Use this for initialization
+	void Start () {
+		currentDelay = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Jump") && currentDelay <= 0.001) {
+			ShotMagic();
+			currentDelay = delay;
+		}	
+		currentDelay -= Time.deltaTime;
+		if (currentDelay < 0)
+			delay = 0;
+	}
+
+	void ShotMagic(){
+		MagicBulletController magicBallClone = 
+			Instantiate(magicBall, transform.position, transform.rotation) as MagicBulletController;
+	}
+}
