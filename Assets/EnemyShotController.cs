f2d2f4147a3f00ae178db7908d66122ff0af28ba
winsxx <13512071@std.stei.itb.ac.id>
2015-03-27 21:48:06 +0700

    Reset to enemy can die

---
+++ Assets/EnemyShotController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class EnemyShotController : MonoBehaviour {
+
+	AIRig rig = null;
+
+	// Use this for initialization
+	void Start () {
+		rig = gameObject.GetComponentInChildren<AIRig>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Bullet") {
+			int health = rig.AI.WorkingMemory.GetItem<int>("myHealth");
+			rig.AI.WorkingMemory.SetItem<int>("myHealth", health-25);
+		}
+	}
+}
--- Assets/UnityChan/Scripts/ThrowScript.cs
+++ Assets/UnityChan/Scripts/ThrowScript.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+
 public class ThrowScript : MonoBehaviour {
+
     public GameObject Bomb;
     public GameObject Dirt;
-    public GameObject WhityBomb;
 
 	// Use this for initialization
 	void Start () {
@@ -14,24 +15,11 @@ public class ThrowScript : MonoBehaviour {
 	void Update () {
         if (Input.GetButton("Fire1"))
         {
-            Vector3 posisi_skrg=transform.position + 7.0f * transform.forward;
-            GameObject bom = Instantiate(Bomb, posisi_skrg, transform.rotation) as GameObject;
-            StartCoroutine(efekLedak(4.5f, posisi_skrg));
-            Destroy(bom, 5);            
+            Instantiate(Bomb, transform.position + 7.0f * transform.forward, transform.rotation);
         }
         else if (Input.GetButton("Fire2"))
         {
             Instantiate(Dirt, transform.position + 7.0f * transform.forward, transform.rotation);
         }
 	}
-
-    IEnumerator efekLedak(float waktu,Vector3 posisi)
-    {
-        for (float it = 0; it < waktu; it += Time.deltaTime)
-        {
-            yield return null;
-        }
-        GameObject efek_bom = Instantiate(WhityBomb, posisi, transform.rotation) as GameObject;
-        Destroy(efek_bom, 2);
-    }
 }
