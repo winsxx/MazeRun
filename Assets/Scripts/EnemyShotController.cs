b2f79638328e153ec62e27296ddf63849276c6ab
winsxx <13512071@std.stei.itb.ac.id>
2015-03-27 23:06:29 +0700

    Refactor and arrange files

--- Assets/Scripts/EnemyShotController.cs
+++ Assets/Scripts/EnemyShotController.cs
@@ -4,6 +4,9 @@ using RAIN.Core;
 
 public class EnemyShotController : MonoBehaviour {
 
+	public GameObject attackEffectPrefab;
+	public int healthReducePerAttack;
+
 	AIRig rig = null;
 
 	// Use this for initialization
@@ -19,7 +22,12 @@ public class EnemyShotController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Bullet") {
 			int health = rig.AI.WorkingMemory.GetItem<int>("myHealth");
-			rig.AI.WorkingMemory.SetItem<int>("myHealth", health-25);
+			rig.AI.WorkingMemory.SetItem<int>("myHealth", health-healthReducePerAttack);
+
+			GameObject tempEffect = Instantiate(attackEffectPrefab,other.transform.position, Quaternion.identity) as GameObject;
+
+			Destroy(other.gameObject);
+			Destroy(tempEffect.gameObject, 1f);
 		}
 	}
 }
